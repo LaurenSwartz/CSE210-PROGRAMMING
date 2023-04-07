7d155389a0a786a9366beb9b6c82723c8116a23d
LaurenSwartz <swa21010@byui.edu>
2023-04-07 11:41:32 +0200

    final

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Video video1 = new Video("How to make Fatima Sydow's Koesisters", "Fatima Sydow", 840);
+        video1.AddComment(new Comment("Viewer 1", "Thank you for sharing this recipe, I tried my Moms recipe last week and came out so delicious"));
+        video1.AddComment(new Comment("Viewer 2", "Made my first batch today and it came out brilliant! Thanks Fatima "));
+        video1.AddComment(new Comment("Viewer 3", "Keep it up!"));
+
+        Video video2 = new Video("Chicken and Mashroom pie", "Fatima Sydow", 660);
+        video2.AddComment(new Comment("Viewer 4", "I made this yesterday, came out perfect and delicious"));
+        video2.AddComment(new Comment("Viewer 5", "Love your videos"));
+        video2.AddComment(new Comment("Viewer 6", "You're such an amazing cook"));
+
+        Video video3 = new Video("3 egg milk tart", "Fatima Sydow", 780);
+        video3.AddComment(new Comment("Viewer 7", "Your cooking makes me feel at home"));
+        video3.AddComment(new Comment("Viewer 8", "Just made it now, Aunty Fatima"));
+        video3.AddComment(new Comment("Viewer 9", "This channel is so inspiring"));
+
+        List<Video> videos = new List<Video> { video1, video2, video3 };
+
+        foreach (Video video in videos)
+        {
+            System.Console.WriteLine($"Title: {video._title}");
+            System.Console.WriteLine($"Author: {video._author}");
+            System.Console.WriteLine($"Length: {video._length} seconds");
+            System.Console.WriteLine($"Number of comments: {video.GetNumberofComments()}");
+
+            System.Console.WriteLine("Comments: ");
+            foreach (Comment comment in video.GetComments())
+            {
+                System.Console.WriteLine($"- {comment._name}: {comment._text}");
+            }
+            System.Console.WriteLine();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Product product1 = new Product("Tv", 1, 3000, 1);
+        Product product2 = new Product("Office Table", 1, 30, 1);
+        Product product3 = new Product("Wifi Router", 3, 50, 1);
+        Product product4 = new Product("Ups", 4, 200, 1);
+
+        Address address1 = new Address("1 Hull street", "Johannesburg", "JHB", "RSA");
+        Address address2 = new Address("Plot 97, 7th ave", "Johannesburg", "JHB", "RSA");
+
+        Customer customer1 = new Customer("Lauren Swartz", address1);
+        Customer customer2 = new Customer("Maxine Marais", address2);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product3);
+        order2.AddProduct(product4);
+
+        DisplayOrderDetails(order1);
+        System.Console.WriteLine("\n------------------\n");
+        DisplayOrderDetails(order2);
+    }
+
+    static void DisplayOrderDetails(Order order)
+    {
+        System.Console.WriteLine(order.GetPackingLabel());
+        System.Console.WriteLine(order.GetShippingLabel());
+        System.Console.WriteLine($"Total Price: ${order.CalculateTotalCost():0.00}");
     }
 }
